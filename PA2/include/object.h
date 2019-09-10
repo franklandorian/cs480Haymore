0266b239fb19b4a8ebf86510a01ddd5a700b58d1
Franklin Haymore <fhaymore@nevada.unr.edu>
2019-09-10 11:59:41 -0700

    fixed header this time for sure. created PA2

---
+++ PA2/include/object.h
@@ -0,0 +1,29 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+
+class Object
+{
+  public:
+    Object();
+    ~Object();
+    void Update(unsigned int dt);
+    void Render();
+
+    glm::mat4 GetModel();
+
+  private:
+    glm::mat4 model;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+    int rotationMod;
+
+
+    float angle;
+};
+
+#endif /* OBJECT_H */
